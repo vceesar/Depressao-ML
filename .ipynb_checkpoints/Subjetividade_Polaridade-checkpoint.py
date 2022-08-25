c44baaf6a1c162053f804fb5caad22903fd44d27
vceesar <victorc1104@gmail.com>
2022-08-25 01:04:47 -0300

    algoritmos+seaborn

--- .ipynb_checkpoints/Subjetividade_Polaridade-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import textblob from TextBlob
-
-def extrair_subjetividade(txt):
-    return Textblob(txt).sentiment.subjectivity
-
-def extrair_polaridade(txt):
-    return TextBlob(txt).sentiment.polarity
